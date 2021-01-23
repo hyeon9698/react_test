5d1fcebdd7c52ed27676ed9a4ad3f9a796432fb4
DonghyeonCho <hyeon9698@naver.com>
2021-01-23 06:13:47 +0000

    react-build

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import React, { Component } from 'react';
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +20,37 @@ function App() {
       </header>
     </div>
   );
+}*/
+class App extends Component {
+    render(){
+        return(
+        <div className="App">
+               hello
+        </div>);
+    }
 }
-
+/*<header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+*/
 export default App;
+
+
+
+
+
+
+
+
+
