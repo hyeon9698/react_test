977e7b0abb23c3673d17a9f96b0e9a334616eb27
DonghyeonCho <hyeon9698@naver.com>
2021-01-23 07:12:04 +0000

    react organize

---
+++ react-app/src/components/TOC.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+class TOC extends Component {
+    render(){
+        return(
+            <nav>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                </ul>
+            </nav>
+        );
+    }
+}
+export default TOC;
